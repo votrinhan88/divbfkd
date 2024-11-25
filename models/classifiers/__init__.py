2ce028575f5b64f5787008980d28256e0b0944f4
TRI NHAN VO <s223032975@4gpu-17.ai.deakin.edu.au>
2024-11-25 15:22:01 +1100

    models/classfiers/init

--- models/classifiers/__init__.py
+++ models/classifiers/__init__.py
@@ -1,12 +1,10 @@
 from .alexnet import AlexNet
-from .hintonnet import HintonNet
 from .lenet5 import LeNet5
 from .resnet import ResNet_CIFAR
 from .vgg import VGG
 from .utils import ClassifierTrainer
 
 del alexnet
-del hintonnet
 del lenet5
 del resnet
 del vgg
