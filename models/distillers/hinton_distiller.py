09459942721775fc8621e7691c159d8a76499822
TRI NHAN VO <s223032975@4gpu-17.ai.deakin.edu.au>
2024-11-25 16:26:52 +1100

    Update StandardKD

--- models/distillers/hinton_distiller.py
+++ models/distillers/hinton_distiller.py
@@ -12,32 +12,21 @@ from torch import nn, Tensor
 
 from models.distillers import Distiller
 from utils.metrics import DDPMetric, Mean, CategoricalAccuracy
+from utils.trainers import parse_loss
 
 
 class HintonDistiller(Distiller):
-    """Traditional knowledge distillation scheme, training the student on both
+    """Standard knowledge distillation scheme, training the student on both
     actual data and the soft targets produced by a pre-trained teacher.
 
-    Args:
-    + `teacher`: Pre-trained teacher model. Must return logits.
-    + `student`: To-be-trained student model. Must return logits.
-    + `image_dim`: Dimension of input images. Defaults to `None`, skip to parse \
-        from student.
-
-    Kwargs:
-    + `device`: The desired device of trainer, needs to be declared explicitly  \
-        in case of Distributed Data Parallel training. Defaults to `None`, skip \
-        to automatically choose single-process `'cuda'` if available or else    \
-        `'cpu'`.
-    + `world_size`: The number of processes in case of Distributed Data Parallel\
-        training. Defaults to `None`, skips to query automatically with `torch`.
-    + `master_rank`: Rank of the master process. Defaults to `0`.
+    Kwargs: Additional arguments:
+      + To `Distiller`: `teacher`, `student`, `image_dim`.
+      + To `Trainer`: `device`, `world_size`, `master_rank`.
     
-    Distilling the Knowledge in a Neural Network - Hinton et al. (2015)
+    Distilling the Knowledge in a Neural Network - Hinton et al. (2015).
     DOI: 10.48550/arXiv.1503.02531
     """
-    def compile(
-        self,
+    def compile(self,
         opt:torch.optim.Optimizer,
         loss_fn_distill:bool|Callable[[Any], Tensor]=True,
         loss_fn_label:bool|Callable[[Any], Tensor]=True,
@@ -49,30 +38,22 @@ class HintonDistiller(Distiller):
         """Compile distiller.
 
         Args:
-        + `opt`: Optimizer for student model.
-        + `loss_fn_distill`: Distillation loss function to match student's with \
+          `opt`: Optimizer for student model.
+          `loss_fn_distill`: Distillation loss function to match student's with \
             teacher's prediction. Pass in a custom function or toggle with      \
             `True`/`False`. Defaults to `True`.
-        + `loss_fn_label`: Label loss function to match student's prediction    \
+          `loss_fn_label`: Label loss function to match student's prediction    \
             with actual label. Pass in a custom function or toggle with `True`/ \
             `False`. Defaults to `True`.
-        + `coeff_dt`: Multiplier assigned to distillation loss. Defaults to     \
+          `coeff_dt`: Multiplier assigned to distillation loss. Defaults to     \
             `0.9`.
-        + `coeff_lb`: Multiplier assigned to label loss. Defaults to `0.1`.
-        + `temperature`: Temperature for softening probability distributions.   \
+          `coeff_lb`: Multiplier assigned to label loss. Defaults to `0.1`.
+          `temperature`: Temperature for softening probability distributions.   \
             Larger temperature gives softer distributions. Defaults to `10`.
-
-        Kwargs:
-        + `loss_fn_test`: Test loss function to test student's prediction with  \
-            ground truth label, of type `bool` or custom. `True`: use the cross-\
-            entropy loss, `False`: skip measuring test loss, or pass in a custom\
-            loss function. Defaults to `True`.
-        + `sync_ddp_metrics`: Flag to synchronize metrics during Distributed    \
-            Data Parallel training. Can be of type `bool` or dict of `bool`,    \
-            accessible with the keys `'train'`|`'val'`. Defaults to `None` for  \
-            off for training and on for validation. Warning: Turning this on    \
-            will incur extra communication overhead.
-
+        
+        Kwargs: Additional arguments:
+          + To `Distiller().compile`: `loss_fn_test`.
+          + To `Trainer().compile`: `sync_ddp_metrics`.
         """
         super().compile(**kwargs)
         self.opt = opt
@@ -82,21 +63,10 @@ class HintonDistiller(Distiller):
         self.coeff_lb = coeff_lb
         self.temperature = temperature
 
-        # Config distillation loss
-        if self.loss_fn_distill is True:
-            self._loss_fn_distill = nn.CrossEntropyLoss()
-        elif self.loss_fn_distill is False:
-            self._loss_fn_distill = lambda *args, **kwargs:torch.tensor(0)
-        else:
-            self._loss_fn_distill = self.loss_fn_distill
-        # Config student loss
-        if self.loss_fn_label is True:
-            self._loss_fn_label = nn.CrossEntropyLoss()
-        elif self.loss_fn_label is False:
-            self._loss_fn_label = lambda *args, **kwargs:torch.tensor(0)
-        else:
-            self._loss_fn_label = self.loss_fn_label
-
+        # Config losses
+        self._loss_fn_distill = parse_loss(loss_arg=loss_fn_distill, default=nn.CrossEntropyLoss())
+        self._loss_fn_label = parse_loss(loss_arg=loss_fn_label, default=nn.CrossEntropyLoss())
+        
         # Metrics
         if self.loss_fn_distill is not False:
             self.train_metrics.update({'loss_dt': Mean()})
@@ -130,11 +100,11 @@ class HintonDistiller(Distiller):
         soft_prob_T = (logits_T/self.temperature).softmax(dim=1)
         soft_logits_S = (logits_S/self.temperature).log_softmax(dim=1)
         # Distillation loss: Not multiplying with T^2 gives slightly better results
-        loss_distil = self._loss_fn_distill(input=soft_logits_S, target=soft_prob_T) * self.temperature**2
+        loss_distill = self._loss_fn_distill(input=soft_logits_S, target=soft_prob_T) * self.temperature**2
         # Label loss: standard loss with training data
         loss_label = self._loss_fn_label(logits_S, label)
-        loss_S = (
-            + self.coeff_dt*loss_distil
+        loss_S:Tensor = (
+            + self.coeff_dt*loss_distill
             + self.coeff_lb*loss_label
         )
         
@@ -147,7 +117,7 @@ class HintonDistiller(Distiller):
         with torch.inference_mode():
             # Metrics
             if self.loss_fn_distill is not False:
-                self.train_metrics['loss_dt'].update(new_entry=loss_distil)
+                self.train_metrics['loss_dt'].update(new_entry=loss_distill)
             if self.loss_fn_label is not False:
                 self.train_metrics['loss_lb'].update(new_entry=loss_label)
             if ((self.loss_fn_distill is not False)
