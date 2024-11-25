617be3fd22b4e21f7348025f1cc0c69c3065bb89
TRI NHAN VO <s223032975@4gpu-17.ai.deakin.edu.au>
2024-11-25 16:30:52 +1100

    Update Classifiers init

--- models/classifiers/__init__.py
+++ models/classifiers/__init__.py
@@ -1,11 +1,11 @@
 from .alexnet import AlexNet
 from .lenet5 import LeNet5
-from .resnet import ResNet_CIFAR
+from .resnetcifar import ResNet_CIFAR
 from .vgg import VGG
 from .utils import ClassifierTrainer
 
 del alexnet
 del lenet5
-del resnet
+del resnetcifar
 del vgg
 del utils
